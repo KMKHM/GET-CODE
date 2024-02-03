8c4cc383dcba6dff49f1367990f2d2192e86c54a
KMK_HM <kyun9151@gmail.com>
2024-02-03 15:38:58 +0900

    feat: validation 추가

--- src/main/java/com/getcode/controller/member/MemberController.java
+++ src/main/java/com/getcode/controller/member/MemberController.java
@@ -3,6 +3,7 @@ package com.getcode.controller.member;
 import com.getcode.controller.member.request.MemberCreateRequest;
 import com.getcode.service.member.MemberService;
 import com.getcode.service.member.response.MemberResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/sign-up")
-    public ResponseEntity<MemberResponse> signup(@RequestBody MemberCreateRequest req) {
+    public ResponseEntity<MemberResponse> signup(@Valid @RequestBody MemberCreateRequest req) {
         return ResponseEntity.status(HttpStatus.CREATED).body(memberService.signup(req.toServiceRequest()));
     }
 }
--- src/main/java/com/getcode/controller/member/request/MemberCreateRequest.java
+++ src/main/java/com/getcode/controller/member/request/MemberCreateRequest.java
@@ -1,6 +1,9 @@
 package com.getcode.controller.member.request;
 
 import com.getcode.service.member.request.SignupDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,8 +11,17 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class MemberCreateRequest {
+
+    @NotBlank
+    @Pattern(regexp="^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$", message="이메일 주소 양식을 확인해주세요")
     private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상입니다.")
     private String password;
+
+    @NotBlank
+    @Size(min = 2, message = "닉네임 길이는 최소 2자 이상입니다.")
     private String nickname;
 
     @Builder
